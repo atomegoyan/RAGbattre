65765232a35979094c71425f9a2d9579a84e1862
atomegoyan <aurelienpellet@hotmail.fr>
2025-06-18 18:47:11 +0200

    add notebook

--- entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# entrypoint.sh
-
-# Take ownership of the Hugging Face cache directory
-chown -R appuser:appuser /home/appuser/.cache/huggingface
-
-# Execute the original command as appuser
-exec gosu appuser "$@"
