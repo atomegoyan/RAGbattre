07a123abb1307b56cd4ea8ae60b0310dd97318e8
Aurélien PELLET <aurelienpellet@hotmail.fr>
2025-06-24 14:19:38 +0200

    update

--- scripts/embeddings/generate_embeddings_cs1.py
+++ scripts/embeddings/generate_embeddings_cs1.py
@@ -11,7 +11,7 @@ DATA_DIR = os.path.join(BASE_DIR,'data')
 path_to_embeddings = os.path.join(DATA_DIR,"corpus_splitted_cs1")
 
 client = chromadb.PersistentClient(path = os.path.join(DATA_DIR,"embeddings_cs1"))
-st_ef = embedding_functions.SentenceTransformerEmbeddingFunction(model_name="Alibaba-NLP/gte-multilingual-base",trust_remote_code=True,device="cuda",normalize_embeddings=True)
+st_ef = embedding_functions.SentenceTransformerEmbeddingFunction(model_name="Alibaba-NLP/gte-multilingual-base",trust_remote_code=True,device="cpu",normalize_embeddings=True)
 
 seances = sorted(os.listdir(path_to_embeddings))
 
